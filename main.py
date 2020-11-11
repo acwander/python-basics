3a442c680463fc8dda2370ee1a1c2e61f9019e55
Alex Wander <alexcwander@gmail.com>
2020-11-11 11:34:06 -0600

    Basic Data Types (int, float, complex)

---
+++ main.py
@@ -0,0 +1,29 @@
+# Fundamental Data Types
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4)) # 0.5 float
+
+print(2 ** 2)
+print(5 // 4)
+print(6 % 4)
+
+# math functions
+print(round(3.1))
+print(round(3.9))
+
+print(abs(-20))
+
+# operator precedent
+# ()
+# **
+# * /
+# + -
+print((20 - 3) + 2 ** 2)
+
+# complex is another data type along with int and float, less common.
+
+# binary numbers
+print(bin(5))
+print(int('0b101', 2))
+
